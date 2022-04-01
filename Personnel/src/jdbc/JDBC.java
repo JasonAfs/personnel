6a879697cb477298c415c8a822300eb415e4f3de
JasonAfs <jason.afonso.fernandes@gmail.com>
2022-04-01 14:53:05 +0200

    Update JDBC.java

--- Personnel/src/jdbc/JDBC.java
+++ Personnel/src/jdbc/JDBC.java
@@ -46,8 +46,7 @@ public class JDBC implements Passerelle
 			String requete = "select * from ligue";
 			Statement instruction = connection.createStatement();
 			ResultSet ligues = instruction.executeQuery(requete);
-			System.out.println(ligues.next()+"salut");
-			while (ligues.next())
+			while (ligues.next())ffffff
 			{
 				Ligue ligue = gestionPersonnel.addLigue(ligues.getInt(1), ligues.getString(2));
 				requete = "Select * from employe where ligue = "+ ligue.getId();
